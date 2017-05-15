35a792fa7a2ca6e38725a8453e2b942848448326
DESKTOP-7UNOAH6\lunia <luniana44@gmail.com>
2017-05-15 18:45:31 +0900

    solve error header.blade.php

--- resources/views/partials/mySubNavi.blade.php
+++ resources/views/partials/mySubNavi.blade.php
@@ -1,7 +1,7 @@
 <div class="default-padding"></div>
 
 <ul class="nav nav-pills my-sub-navigation">
-        <li role="presentation" class="active"><a href="#">최근본 소설</a></li>
+        <li role="presentation" class="active"><a href="#">최근 본 소설</a></li>
         <li role="presentation"><a href="#">관심소설</a></li>
         <li role="presentation"><a href="#">책갈피</a></li>
         <li role="presentation"><a href="#">내작품</a></li>
