17ec5916625f80910fcce0cd49c02e9c23701354
DESKTOP-7UNOAH6\lunia <luniana44@gmail.com>
2017-05-15 20:16:38 +0900

    default background_set setting, style.css changed

--- resources/views/background/historyTable/history_table_view.blade.php
+++ resources/views/background/historyTable/history_table_view.blade.php
@@ -6,7 +6,9 @@
 
 
 @section('content') 
-		
+		<div class="col-xs-6 col-sm-4 col-md-4 background_set" style= "background-color : #e8d6b3" >.col-xs-12 .col-sm-6 .col-md-8</div>
+		<div class="col-xs-5 col-sm-5 col-md-5 background_set" >.col-xs-12 .col-sm-6 .col-md-8</div>
+		<div class="col-xs-3 col-sm-2 col-md-2 background_set background_tag" >.col-xs-12 .col-sm-6 .col-md-8</div>
 
 	{{-- 태그 div.row 닫는 태그 --}}
 	</div>
