c1b680f16eb1679bc1b4b4e2fbeea59db0e23997
DESKTOP-7UNOAH6\lunia <luniana44@gmail.com>
2017-05-16 21:13:06 +0900

    char/things/time/map/relation basic view

--- resources/views/background/things/things_view.blade.php
+++ resources/views/background/things/things_view.blade.php
@@ -6,9 +6,107 @@
 
 
 @section('content') 
-		<div class="col-xs-6 col-sm-4 col-md-4 height-max-set" style= "background-color : #e8d6b3" >.col-xs-12 .col-sm-6 .col-md-8</div>
-		<div class="col-xs-5 col-sm-5 col-md-5 height-max-set" >.col-xs-12 .col-sm-6 .col-md-8</div>
-		<div class="col-xs-3 col-sm-2 col-md-2 height-max-set background_tag" >.col-xs-12 .col-sm-6 .col-md-8</div>
+		<div class="col-xs-6 col-sm-4 col-md-4 height-max-set" style= "background-image:url('/img/cork.jpg')" >
+
+		</div>
+		<div class="col-xs-5 col-sm-5 col-md-5 height-max-set" >
+			<h3>사물 등록</h3>
+			{{-- 사물 이름 등록 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-2 control-label" for="formGroupInputLarge">이름</label>
+					<div class="col-sm-10">
+					<input class="form-control" type="text" id="formGroupInputLarge" placeholder="Large input">
+					</div>
+				</div>
+			</form>
+			{{-- 사물 내용 등록 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-2 control-label" for="formGroupInputLarge">내용</label>
+					<div class="col-sm-10">
+					<textarea class="form-control" rows="3"></textarea>
+					</div>
+				</div>
+			</form>
+			{{-- 사물 분류 등록 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-2 control-label" for="formGroupInputLarge">분류</label>
+					<div class="col-sm-10">
+					<input class="form-control" type="text" id="formGroupInputLarge" placeholder="Large input">
+					</div>
+				</div>
+			</form>
+			{{-- 사물 소유자 등록 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-2 control-label" for="formGroupInputLarge">소유자</label>
+					<div class="col-sm-10">
+					<input class="form-control" type="text" id="formGroupInputLarge" placeholder="Large input">
+					</div>
+				</div>
+			</form>
+			{{-- 사물 추가사항 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-3 control-label" for="formGroupInputLarge">추가사항</label>
+					<div class="col-sm-7">
+						<input class="form-control" type="text" id="formGroupInputSmall" placeholder="Small input">
+					</div>
+					<div class="col-sm-2">
+						<i class="fa fa-plus-circle" aria-hidden="true" style="font-size:200%"></i>
+					</div>
+				</div>
+			</form>
+			{{-- 이미지 등록 --}}
+			<form class="form-horizontal">
+				<div class="form-group form-group-lg">
+					<label class="col-sm-3 control-label" for="formGroupInputLarge">이미지 등록</label>
+					<div class="col-sm-10">
+						<input type="file" id="exampleInputFile">
+					</div>
+				</div>
+			</form>
+			
+			<button type="submit" class="btn btn-default">등록</button>
+		</div>
+		<div class="col-xs-3 col-sm-2 col-md-2 height-max-set background_tag" >
+			{{-- add_tag.blade.php 구현하고, 컨트롤러로 div안에 불러오는 형식으로 변경 할 것. --}}
+			<form class="form-horizontal main-navigation">
+				<div class="form-group form-group-sm">
+					<br>
+					<label class="col-sm-2 control-label" for="formGroupInputSmall">검색</label>
+					<div class="col-sm-10">
+						<input class="form-control" type="text" id="formGroupInputSmall" placeholder="Small input">
+					</div>
+					<br>
+					<br>
+					<button type="submit" class="btn btn-default">검색</button>
+				</div>
+			</form>
+			<div class="row">
+				<h3>태그 등록</h3>
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title">태그 이름</h3>
+					</div>
+					<div class="panel-body">
+						<input type="text" class="form-control" placeholder="Text input">
+					</div>
+				</div>
+
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title">태그 내용</h3>
+					</div>
+					<div class="panel-body">
+						<textarea class="form-control" rows="3"></textarea>
+					</div>
+				</div>
+				<button type="submit" class="btn btn-default">등록</button>
+			</div>
+		</div>
 
 	{{-- 태그 div.row 닫는 태그 --}}
 	</div>
