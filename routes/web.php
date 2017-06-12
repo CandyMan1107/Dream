9bafab70cab68304d5a7ac9d075e7a30eb687e3c
daegunkor <daegunkor@gmail.com>
2017-06-12 09:39:24 +0900

    LDG git ignore composer.lock and fix web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -72,8 +72,7 @@ Route::get('/write_novel/get_novel_info', "writeNovelController@getNovelInfo");
 Route::get('/write_novel/my_novel', "writeNovelController@myNovelView");
 
 // 나의소설-에피소드
-Route::get('/write_novel/novel_episode/{data}', "writeNovelController@novelEpisodeView")->with("data",$data);
-
+Route::get('/write_novel/novel_episode/{data}', "writeNovelController@novelEpisodeView");
 // 이미지 등록부
 // 커버 이미지 등록
 Route::post('/write_novel/addCover', "UpImgController@uploadImg");
