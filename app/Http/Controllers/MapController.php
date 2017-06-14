2c9fff644c1ef234813e592caa1fd0fa732f3fd9
yongshinkim <aazza1@naver.com>
2017-06-14 23:48:11 +0900

    KYS map Image Upload complete!!

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -5,21 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use DB;
-use App\Map;
 use File;
+use App\Map;
 
 class MapController extends Controller
 {
     public function index() {
-        $imgRoot = "img/background/mapImg/";
-        $img_src = DB::select("select * from maps");
-        $tasks = array(
-            "id"   => $imgRoot
-            // "img_src"  => $img_src["img_src"]
-        );
-        
-        return view('background.map.map_view')->with('tasks', $tasks);
+        $imgRoot = "img/background/mapImg";
+        $img_src = DB::select("select * from map_images");
+
+        $data = array();
+
+        foreach ($img_src as $datas) {
+            $d_arr = [
+                "id"        => $datas->id,
+                "img_src"   => $imgRoot."/".$datas->img_src
+            ];    
+            array_push($data, $d_arr);
+        }
+
+        return view('background.map.map_view')->with("data", $data);
     }
+
+    // 맵 목록
+    public function mapList() {
+        $data = array();
+        $map_src = DB::select("select * from maps");
+
+        foreach ($map_src as $datas) {
+            $d_arr = [
+                "id"        => $datas->id
+            ];    
+            array_push($data, $d_arr);
+        }
+        
+        return view('background.map.map_view')->with("data", $data);
+    }
+
 
 
     //지도 이미지 업로드
@@ -30,7 +52,7 @@ class MapController extends Controller
         $fileName = date("Y").date("m").date("d").date("s").$file->getClientOriginalName();
         $file->move($destinationPath, $fileName);
 
-        DB::table("maps")->insert([
+        DB::table("map_images")->insert([
             // "id" => 1,     // 유저 아이디는 임의값
             "img_src" => $fileName
         ]);
