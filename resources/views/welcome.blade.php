153c0af577e476b3963104d3d6944b53fe1b8ac6
JHM <yerriel96@gmail.com>
2017-06-19 10:16:50 +0900

    jhm welcome UPDATE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,72 +40,6 @@
 	</div>--}}
 	{{--CTA1 END--}}
 	<div id="default-padding-mid"></div>
-<<<<<<< HEAD
-	{{-- SERVICE START --}}
-	<div id="today-novel" class="section-padding">
-		<div class="container">
-			<div class="row">
-				<div class="page-title text-center">
-					<h1><a class="noDeco" href="/novel/today_novel_by_day">오늘의 웹소설</a></h1>
-					<p>오늘의 <br>HOT한 소설! </p>
-					<hr class="pg-titl-bdr-btm"></hr>
-				</div>
-
-				@for ($i = 0; $i < 3; $i++)
-					<div class="col-md-4">
-						<a href="/novel/novel_info/{{ $data[$i]['id'] }}">
-							<div class="service-box">
-								<div class="service-icon"><img src="upload/images/{{ $data[$i]['cover_img_src'] }}" /></div>
-								<div class="service-text">
-									<h5>{!! $data[$i]['genre'] !!}</h5>
-									<h3>{!! $data[$i]['title'] !!}</h3>
-									<p> <i class="fa fa-pencil"></i> 글반죽</p>
-									<p> <b>{!! $data[$i]['summary_intro'] !!}</b></p>
-								</div>
-							</div>
-						</a>
-					</div>
-				@endfor
-
-			</div>
-		</div>
-	</div>
-	{{--SERVICE END--}}
-
-	{{--PORTFOLIO START--}}
-	<div id="best-novel" class="section-padding">
-		<div class="container">
-			<div class="row">
-				<div class="page-title text-center">
-					<h1>베스트 웹소설</h1>
-					<p>장르별로 다양한 웹소설 <br>독자들이 선택한 베스트 웹소설은? </p>
-					<hr class="pg-titl-bdr-btm"></hr>
-				</div>
-				<div class="port-sec">
-					<div class="col-md-12 fil-btn text-center">
-							<div class="filter wrk-title active" data-filter="all">전체</div>
-							<div class="filter wrk-title" data-filter=".romance">로맨스</div>
-							<div class="filter wrk-title" data-filter=".fantasy">판타지</div>
-							<div class="filter wrk-title lst-cld" data-filter="horror">호러</div>
-					</div>
-					<div id="Container">
-
-						@for ($i = 0; $i < count($data); $i++)
-							<div class="filimg mix {{ $data[$i]['genre'] }} col-md-4 col-sm-4 col-xs-12" data-myorder="2">
-								<a href="/novel/novel_info/{{ $data[$i]['id'] }}">
-									<img src="upload/images/{{ $data[$i]['cover_img_src'] }}" class="img-responsive">
-								</a>
-							</div>
-						@endfor
-
-
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	{{--PORTFOLIO END--}}
-=======
 	
 	{{--today & best NOVEL START--}}
 	@php
@@ -115,7 +49,6 @@
 	echo MainController::bestNovel($data);
 	@endphp
 	{{--today & best NOVEL END--}}
->>>>>>> 0b166776d036af4c5af214d087a5271948ca6173
 
 	{{--EVENT START--}}
 	<div id="event" class="section-padding">
