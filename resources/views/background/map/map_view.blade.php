942fb38c73966fce7b61d67b2f0bb9c8effc3d36
daegunkor <daegunkor@gmail.com>
2017-06-22 21:19:37 +0900

    LDG map view

--- resources/views/background/map/map_view.blade.php
+++ resources/views/background/map/map_view.blade.php
@@ -639,7 +639,7 @@ rant @extends('layouts.master')
               contentType: false,
               success: function(data){
 								var imgEle = "";
-								alert(data.imgPath);
+								//alert(data.imgPath);
 								var imgSrc = "{{URL::asset('/')}}" + data.imgPath;
 								imgEle += "<img id='img" + data.imgId + "' class='img-cell' src='" + imgSrc + "'>";
 								defImagePattern(data.imgId, imgSrc)
