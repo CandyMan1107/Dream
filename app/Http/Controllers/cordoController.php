96c91bbf83d9e068325a2a25f2652acccaedad85
KYSS <aazza1@naver.com>
2017-06-22 19:24:32 +0900

    Server commit

--- app/Http/Controllers/cordoController.php
+++ app/Http/Controllers/cordoController.php
@@ -62,7 +62,7 @@ class cordoController extends Controller
     ->select("novel_writers.novel_id", "title", "summary_intro","cover_img_src","genre","name")
     ->join("novel_writers","novels.id", "=", "novel_writers.novel_id")
     ->join("users","novel_writers.user_id", "=", "users.id")
-    //->where("novels.title","like","%무%")->get();
+    //->where("novels.title","like","%서%")->get();
     ->where("novels.title","like","%".$searchContent."%")->get();
     return $novelInfo;
   }
@@ -86,4 +86,56 @@ class cordoController extends Controller
     $task = DB::table("novel_episodes")->where('belong_to_novel','=',$id)->where('is_notice','=','1')->get();
     return $task;
   }
+  //로그인 하기
+  public function getUsersInfo(Request $request){
+    $user_id = $request->input('id');
+    $password = $request->input('pw');
+    $task = DB::table("users")->where('user_id','=',$user_id)->where('password','=', $password)->get();
+    return $task;
+  }
+  //아이디 중복체크
+  public function idCheck(Request $request){
+    $user_id = $request->input('id');
+    $task = DB::table("users")->where('user_id', '=', $user_id)->get()->count();
+    return $task;
+  }
+  //아이디 찾기
+  public function idSearch(Request $request){
+    $emailInfo = $request->input('email');
+    $task = DB::table('users')->select('user_id')->where('email','=',$emailInfo)->get();
+    return $task;
+  }
+  //비밀번호 찾기
+  public function pwSearch(Request $request){
+    $user_id = $request->input('id');
+    $emailInfo = $request->input('email');
+    $task = DB::table('users')->select('password')->where('user_id', '=', $user_id)->where('email','=',$emailInfo)->get();
+    return $task;
+  }
+  //회원 정보 입력
+  public function insertUserInfo(Request $request){
+      $id = $request->input('user_id');
+      $nick = $request->input('nickname');
+      $email = $request->input('email');
+      $pw = $request->input('user_pw');
+      DB::table('users')->insert([
+        "user_id" => $id,
+        "name" => $nick,
+        "email" => $email,
+        "password" => $pw
+      ]);
+    }
+
+  //캐릭터 목록 가져오기
+  public function getCharacters(Request $request){
+    $tasks = DB::table("characters")->get();
+    return $tasks;
+  }
+
+  //캐릭터 정보 가져오기
+//   public function getCharactersInfo(){
+//     $id = $request->input('id');
+//     $task = DB::table("characters")->where('id','=',$id)->get();
+//     return $task;
+//   }
 }
