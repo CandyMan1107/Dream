7eda3986bd15daba23aff80c151d48ec7e0d0571
daegunkor <daegunkor@gmail.com>
2017-06-25 20:35:08 +0900

    LDG tag editor bug refix

--- resources/views/write_novel/write_episode_view.blade.php
+++ resources/views/write_novel/write_episode_view.blade.php
@@ -1285,6 +1285,51 @@
       history.go(-1);
     });
 
+
+    $('.episode-editor-div').on('paste', function (e) {
+      var pasteData = e.originalEvent.clipboardData.getData('text');
+
+      e.preventDefault();
+      pasteData = pasteData.replace(/(?:\r\n|\r|\n)/g, '<br />');
+      //e.originalEvent.clipboardData.setData('text/plain', pasteData);
+
+      pasteHtmlAtCaret(pasteData);
+    });
+    function pasteHtmlAtCaret(html) {
+      var sel, range;
+      if (window.getSelection) {
+          // IE9 and non-IE
+          sel = window.getSelection();
+          if (sel.getRangeAt && sel.rangeCount) {
+              range = sel.getRangeAt(0);
+              range.deleteContents();
+
+              // Range.createContextualFragment() would be useful here but is
+              // non-standard and not supported in all browsers (IE9, for one)
+              var el = document.createElement("div");
+              el.innerHTML = html;
+              var frag = document.createDocumentFragment(), node, lastNode;
+              while ( (node = el.firstChild) ) {
+                  lastNode = frag.appendChild(node);
+              }
+              range.insertNode(frag);
+
+              // Preserve the selection
+              if (lastNode) {
+                  range = range.cloneRange();
+                  range.setStartAfter(lastNode);
+                  range.collapse(true);
+                  sel.removeAllRanges();
+                  sel.addRange(range);
+              }
+          }
+      } else if (document.selection && document.selection.type != "Control") {
+          // IE < 9
+          document.selection.createRange().pasteHTML(html);
+      }
+    }
+
+
     // 저장 버튼
     $(".episode-save").on("click",function(){
       // 소설 정보 변수화
