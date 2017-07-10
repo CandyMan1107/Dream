40ade88ac686cf91faeb7055f0736bf91efbf9fa
JHM <yerriel96@gmail.com>
2017-07-10 10:49:42 +0900

    jhm blog start

---
+++ resources/views/writer_blog/blog_main.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@include('writer_blog.blogTopMenu')
+
+@section('content')
+    <div class="default-padding"></div>
+
+    
+    
+
+
+
+
+
+
+    <div id="default-padding-big"></div>
+
+    {{-- JHM STYLE --}}
+    <link rel="stylesheet" href="/css/jhm-style.css">
+	{{-- JHM SCRIPT --}}
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="/js/JHM-Custom/jhm-todayNovel-custom.js"></script>
+	<script src="/js/jquery-3.2.0.js"></script>
+	<script src="/js/jquery.easing.min.js" type="text/javascript"></script>
+	<script src="/js/jquery.mixitup.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/js/slick.js"></script>
+	<script type="text/javascript" src="/js/custom.js"></script>
+@endsection
